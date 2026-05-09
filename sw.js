1e0e842b554a49afdc3dcf050c22c11bf9f5a540
cristiancroci <info.cristiancroci@gmail.com>
2026-05-09 05:34:58 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,3 +5,9 @@ self.addEventListener("install", () => {
 self.addEventListener("activate", (event) => {
   event.waitUntil(self.clients.claim());
 });
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
